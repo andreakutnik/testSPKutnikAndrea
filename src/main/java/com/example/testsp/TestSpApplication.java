463afe01e50caa02931ce444d31a8f27dc52c83e
andreakutnik <ANDREA.KUTNIK00@E-UVT.RO>
2023-11-23 10:43:40 +0200

    Initial commit

---
+++ src/main/java/com/example/testsp/TestSpApplication.java
@@ -0,0 +1,13 @@
+package com.example.testsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testsp/TestSpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
